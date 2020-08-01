2262c951621ca6735c9edc1438beaa8202870080
Lohrainee <lohrainee.s@gmail.com>
2020-08-01 08:06:29 -0300

    Inserção de usuário com POST

--- src/main/java/com/datagrupo/workshopmongo/services/UserService.java
+++ src/main/java/com/datagrupo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.datagrupo.workshopmongo.domain.User;
+import com.datagrupo.workshopmongo.dto.UserDTO;
 import com.datagrupo.workshopmongo.repository.UserRepository;
 import com.datagrupo.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,12 +22,18 @@ public class UserService {
 		
 	}
 	
-	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));			
 		} 
 	
-	} 
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDTO objDto) {
+		return new User (objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+} 
 
 
