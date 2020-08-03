d7c99d47bc76e67c3da95fb72104b3ec2f188606
Lohrainee <lohrainee.s@gmail.com>
2020-08-03 11:51:34 -0300

    Consulta com vários critérios

--- src/main/java/com/datagrupo/workshopmongo/resources/util/URL.java
+++ src/main/java/com/datagrupo/workshopmongo/resources/util/URL.java
@@ -2,6 +2,9 @@ package com.datagrupo.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class URL {
 	
@@ -14,4 +17,13 @@ public class URL {
 		}
 	}
 
+	public static Date convertDate (String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat ("yyy-M-dd");
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
+	}
+	
 }
