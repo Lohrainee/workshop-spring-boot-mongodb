630de364cf6ea07ae4fc20d9da9622fe2d4c15a2
Lohrainee <lohrainee.s@gmail.com>
2020-08-03 10:55:05 -0300

    Consulta simples com query methods

--- src/main/java/com/datagrupo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/datagrupo/workshopmongo/repository/PostRepository.java
@@ -1,13 +1,15 @@
 package com.datagrupo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.datagrupo.workshopmongo.domain.Post;
-import com.datagrupo.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository <Post, String> {
 
+		List<Post> findByTitleContainingIgnoreCase (String text);
 
 }
